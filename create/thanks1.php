d80d0c9f6e6a3b35c23f1621d5c507c7743513e9
John Moosemiller <itcaughtonj@gmail.com>
2014-10-26 13:36:41 -0500

    Create thanks1.php

---
+++ create/thanks1.php
@@ -0,0 +1,39 @@
+<?php
+$con=mysqli_connect("localhost","root","Sandman1","prettymaker");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// escape variables for security
+$theme = mysqli_real_escape_string($con, $_POST['theme']);
+$title = mysqli_real_escape_string($con, $_POST['title']);
+$description = mysqli_real_escape_string($con, $_POST['description']);
+$background = mysqli_real_escape_string($con, $_POST['background']);
+$s1h = mysqli_real_escape_string($con, $_POST['s1h']);
+$s1d = mysqli_real_escape_string($con, $_POST['s1d']);
+$s1l = mysqli_real_escape_string($con, $_POST['s1l']);
+$s2h = mysqli_real_escape_string($con, $_POST['s2h']);
+$s2d = mysqli_real_escape_string($con, $_POST['s2d']);
+$s2l = mysqli_real_escape_string($con, $_POST['s2l']);
+$s3h = mysqli_real_escape_string($con, $_POST['s3h']);
+$s3d = mysqli_real_escape_string($con, $_POST['s3d']);
+$s3l = mysqli_real_escape_string($con, $_POST['s3l']);
+$s4h = mysqli_real_escape_string($con, $_POST['s4h']);
+$s4d = mysqli_real_escape_string($con, $_POST['s4d']);
+$s4l = mysqli_real_escape_string($con, $_POST['s4l']);
+
+$sql="INSERT INTO tips1 (theme, title, description, background, s1h, s1d, s1l, s2h, s2d, s2l, s3h, s3d, s3l, s4h, s4d, s4l)
+VALUES ('$theme', '$title', '$description', '$background', '$s1h', '$s1d', '$s1l', '$s2h', '$s2d', '$s2l', '$s3h', '$s3d', '$s3l', '$s4h', '$s4d', '$s4l')";
+
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
+}
+echo "Thanks for the tip! <br>";
+while($row = mysqli_fetch_array($result)) {
+    echo "Share your tip with <a href=tips.php?id=".$row['uname'].">this link</a>";
+}
+
+
+mysqli_close($con);
+?>
