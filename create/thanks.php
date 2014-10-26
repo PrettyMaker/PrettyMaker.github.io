db514acf616aa664638b99cd75506b3419180cd5
John Moosemiller <itcaughtonj@gmail.com>
2014-10-26 16:25:34 -0500

    Update thanks.php

--- create/thanks.php
+++ create/thanks.php
@@ -30,7 +30,111 @@ if (!mysqli_query($con,$sql)) {
   die('Error: ' . mysqli_error($con));
 }
 
-echo"<html><head><title>".$title."</title></head></html>";
+echo"<html>
+  <head>
+    <title>". $title . "</title>
+    <meta name=\"keywords\" content=\"hacker, fun, FOSS, DMR, OSS\" />
+    <link rel=\"stylesheet\" href=\"css/main.css\">
+    <link rel=\"stylesheet\" href=\"" . $theme . ">
+    <link href=\"http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic\" rel=\"stylesheet\" type=\"text/css\">
+    <style>
+      .intro {
+  display: table;
+  width: 100%;
+  height: 80%;
+  padding: 100px 0;
+  text-align: center;
+  color: #000;
+  background: url(" . $background . ") no-repeat bottom center scroll;  background-color: #000;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
+  -o-background-size: cover;
+}
+
+.intro .intro-body {
+  display: table-cell;
+  vertical-align: middle;
+}
+.intro .intro-body .brand-heading {
+  font-size: 40px;
+}
+.intro .intro-body .intro-text {
+  font-size: 18px;
+}
+    </style>
+
+  
+</head>
+  <body>
+        <!-- Intro Header -->
+    <header class=\"intro\">
+        <div class=\"intro-body\">
+            <div class=\"container\">
+                <div class=\"row\">
+                    <div class=\"col-md-8 col-md-offset-2\">
+                        <strong><h1 >Pretty Maker</h1></strong>
+                        <p class=\"intro-text\">Pretty Maker allows you to quickly make and itterate on simple and effective webpage desing. Pretty Maker allows you to chose from a variety of templates and create an awesome website by telling us what you want to put where. Then, we give you the HTML, so you can change it however you see fit!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Service Section -->
+     <section id=\"services\" class=\"services bg-primary\">
+        <div class=\"container\">
+            <div class=\"row text-center\">
+                <div class=\"col-lg-10 col-lg-offset-1\">
+                    <h2>What you can find here</h2>
+                    <hr class=\"small\">
+                    <div class=\"row\">
+                        <div class=\"col-md-3 col-sm-6\">
+                            <div class=\"service-item\">
+                                <h4>
+                                    <strong>" . $s1h . "</strong>
+                                </h4>
+                                <p>" . $s1d . "</p>
+                                <a href=\"" . $s1l . "\" style=\"color: #fff\"><b>Learn More</b></a>
+                            </div>
+                        </div>
+                        <div class=\"col-md-3 col-sm-6\">
+                            <div class=\"service-item\">
+                                <h4>
+                                    <strong>" . $s2h . "</strong>
+                                </h4>
+                                <p>" . $s2d . "</p>
+                                <a href=\"" . $s2l . "\" style=\"color: #fff\"><b>Learn More</b></a>
+                            </div>
+                        </div>
+                        <div class=\"col-md-3 col-sm-6\">
+                            <div class=\"service-item\">
+                                <h4>
+                                    <strong>" . $s3h . "</strong>
+                                </h4>
+                                <p>" . $s3d . "</p>
+                                <a href=\"" . $s3l . "\" style=\"color: #fff\"><b>Learn More</b></a>
+                            </div>
+                        </div>
+                        <div class=\"col-md-3 col-sm-6\">
+                            <div class=\"service-item\">
+                                <h4>
+                                    <strong>" . $s4h . "</strong>
+                                </h4>
+                                <p>" . $s4d . "</p>
+                                <a href=\"" . $s4l . "\" style=\"color: #fff\"><b>Learn More</b></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.col-lg-10 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+  </body>
+</html>";
 
 mysqli_close($con);
 ?>
